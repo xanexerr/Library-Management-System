b0b9e5db89eda990ced241b02a216fc95c156ade
xanexerr <natthapumin.klammat@gmail.com>
2024-01-06 14:45:21 +0700

    little bit edit

--- logout.php
+++ logout.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php
 session_start();
 session_destroy();
 header("Location: login.php");
 // header("Location: " . $_SERVER['HTTP_REFERER']);
 exit;
-=======
-<?php
-session_start();
-session_destroy();
-header("Location: index.php");
-// header("Location: " . $_SERVER['HTTP_REFERER']);
-exit;
->>>>>>> e8e6f18fc4e84e741c7390069d2e2a561dc90624
 ?>
